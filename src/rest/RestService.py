7579995cce0eca5d9af9df6e68cc2a77eae88866
Dakota Schneider <dakota@hackthetruth.org>
2012-04-07 20:20:35 -0400

    Comments

--- src/rest/RestService.py
+++ src/rest/RestService.py
@@ -2,9 +2,12 @@ import web
 import re
 import uuid
 import sys
-sys.path.append('../src')
 
+# go up a level so we can import stuff
+sys.path.append('../../src')
+# import all the handler modules
 import handler.CitationHandler
+#import handler.SummationHandler
 
 urls = ('/citation/(.*)', 'handler.CitationHandler.CitationHandler'
        )
