dff46ef76ca2bb13e98220a5505aa36f6cdc7d65
Dakota Schneider <dakota@hackthetruth.org>
2012-04-07 20:19:12 -0400

    Changed variable references to self when necessary.

--- src/handler/CitationHandler.py
+++ src/handler/CitationHandler.py
@@ -6,22 +6,28 @@ import web
 
 class CitationHandler():
 
+    def __init__(self):
+        #init DB session
+        self.session = util.DBUtils.DBUtils.getSession()
+        # get format from url parameters
+        url_params  = web.input(format='')
+        self.format = url_params.format
+        # get and parse raw post data
+        data = (web.data())
+        self.data = dict([part.split('=') for part in data.split('&')]) if data != "" else {}
+
+
     def GET(self, cid):
         """Display a single citation or full list if no ID specified
         """
-        session = util.DBUtils.DBUtils.getSession()
         cits = None
         
-        # get format from url parameters
-        url_params  = web.input(format='')
-        format = url_params.format
-        
         if len(cid) <= 0:
-            cits = session.query(models.Citation.Citation).order_by(models.Citation.Citation.id).all()
+            cits = self.session.query(models.Citation.Citation).order_by(models.Citation.Citation.id).all()
         else:
-            cits = session.query(models.Citation.Citation).filter_by(id=cid).all()
+            cits = self.session.query(models.Citation.Citation).filter_by(id=cid).all()
 
-        return util.ResponseUtils.ResponseUtils.createResponse(True, cits, True, format)
+        return util.ResponseUtils.ResponseUtils.createResponse(True, cits, True, self.format)
 
 
 
@@ -29,85 +35,68 @@ class CitationHandler():
         """Create a new citation
             uselessvar is simply a placeholder
         """
-        
-        # get and parse raw post data
-        data = (web.data())
-        data = dict([part.split('=') for part in data.split('&')])
-
-        # create database session
-        session = util.DBUtils.DBUtils.getSession()
-    
+            
         # validate
-        if len(data['content']) > 0 and len(data['url']) > 0:
+        if len(self.data['content']) > 0 and len(self.data['url']) > 0:
             # create database object
-            cit = models.Citation.Citation(data['content'],data['url'])
-            session.add(cit)
+            cit = models.Citation.Citation(self.data['content'], self.data['url'])
+            self.session.add(cit)
             response = "Record created successfully"
             
             # commit change to DB
-            session.commit()
+            self.session.commit()
             status = True
         else:
             response = "Content or URL missing"
             status   = False
 
-        return util.ResponseUtils.ResponseUtils.createResponse(status, response, False)
+        return util.ResponseUtils.ResponseUtils.createResponse(status, response, False, self.format)
 
 
     def PUT(self, cid):
         """Update an existing citation
         """
         
-        # get and parse raw post data
-        data = (web.data())
-        data = dict([part.split('=') for part in data.split('&')])
-
-        # create database session
-        session = util.DBUtils.DBUtils.getSession()
-
         # validate
-        if len(data['content']) > 0 and len(data['url']) > 0:
+        if len(self.data['content']) > 0 and len(self.data['url']) > 0:
             if len(cid) <= 0:
                 response = "Missing parameter (id to be updated)"
                 status   = False
             else:
                 # create database object
-                cit = session.query(models.Citation.Citation).filter_by(id=cid).first()
-                cit.content = data['content']
-                cit.url     = data['url']
-                session.add(cit)
+                cit = self.session.query(models.Citation.Citation).filter_by(id=cid).first()
+                cit.content = self.data['content']
+                cit.url     = self.data['url']
+                self.session.add(cit)
                 response = "Record updated successfully"
 
                 # commit change to DB                
-                session.commit()
+                self.session.commit()
                 status = True
         else:
             response = "Content or URL missing"
             status   = False
 
-        return util.ResponseUtils.ResponseUtils.createResponse(status, response, False)
+        return util.ResponseUtils.ResponseUtils.createResponse(status, response, False, self.format)
 
 
     def DELETE(self, cid):
         """Delete an existing citation
         """
         
-        # create database session
-        session = util.DBUtils.DBUtils.getSession()
-
         # validate
         if len(cid) <= 0:
             response = "Missing parameter (id to be deleted)"
             status   = False
         else:
             # create database object
-            cit = session.query(models.Citation.Citation).filter_by(id=cid).first()
-            session.delete(cit)
+            cit = self.session.query(models.Citation.Citation).filter_by(id=cid).first()
+            self.session.delete(cit)
             
             # commit changes to DB
-            session.commit()
+            self.session.commit()
             status   = True
             response = "Record deleted successfully"
 
-        return util.ResponseUtils.ResponseUtils.createResponse(status, response, False)
+        return util.ResponseUtils.ResponseUtils.createResponse(status, response, False, self.format)
 
