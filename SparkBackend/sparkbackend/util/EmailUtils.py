d9e1aa4dfdf15210c4e219e9288d0fcba54c8c40
Ankit Tandon <ankit.tandon@yahoo.co.in>
2012-06-15 06:53:50 -0700

    Added util file for email/system

---
+++ SparkBackend/sparkbackend/util/EmailUtils.py
@@ -0,0 +1,11 @@
+import pprint
+from sparkbackend.util.Callable import Callable
+
+class EmailsUtils():
+
+    def sendRegistrationMail(userKey,code):
+        """ send registration mail
+        """
+
+    sendRegistrationMail = Callable(sendRegistrationMail)
+
